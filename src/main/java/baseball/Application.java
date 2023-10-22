06cf9d6851e59b5897b221df6ec4a13af8d3b08a
swhyeon98 <smreo75@gmail.com>
2023-10-22 21:06:06 +0900

    refactor: 게임을 종료시키는 로직과 재시작 로직을 각각 'handleGameOver', 'restartOrExitGame' 메서드로 분리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -33,17 +33,7 @@ public class Application {
             printBallAndStrikeCount(strikeCount, ballCount);
 
             if (isGameOver(strikeCount)) {
-                String userChoice = Console.readLine();
-
-                if (userChoice.equals("1")) {
-                    computer = new ArrayList<>();
-                    continue;
-                }
-                if (userChoice.equals("2")) {
-                    return;
-                }
-
-                throw new IllegalArgumentException("1 또는 2 중 하나만 선택하세요.");
+                handleGameOver(computer);
             }
         }
     }
@@ -122,13 +112,24 @@ public class Application {
     }
 
     private static boolean isGameOver(int strikeCount) {
-        if (strikeCount == 3) {
-            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        return strikeCount == 3;
+    }
 
-            return true;
+    private static void handleGameOver(List<Integer> computer) {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        if (restartOrExitGame()) {
+            computer.clear();
         }
+        System.exit(0);
+    }
 
-        return false;
+    private static boolean restartOrExitGame() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String userChoice = Console.readLine();
+
+        if ("1".equals(userChoice)) return true;
+        if ("2".equals(userChoice)) return false;
+
+        throw new IllegalArgumentException("1 또는 2 중 하나만 선택하세요.");
     }
 }
