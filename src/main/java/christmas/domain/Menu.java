7466038eb751c18a63371f523976c4ccc4a5812b
swhyeon98 <smreo75@gmail.com>
2023-11-13 20:09:53 +0900

    feat: 항목과 가격 정보를 관리하는 'Menu' 추가

---
+++ src/main/java/christmas/domain/Menu.java
@@ -0,0 +1,12 @@
+package christmas.domain;
+
+public class Menu {
+
+    private final String name;
+    private final int price;
+
+    public Menu(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+}
