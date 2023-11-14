14ffa9b3a76e56a1577af9c0e35ce38147f2bae6
swhyeon98 <smreo75@gmail.com>
2023-11-14 18:55:41 +0900

    feat: 수량 관련 유효성 검사 기능 추가

--- src/main/java/christmas/domain/OrderItem.java
+++ src/main/java/christmas/domain/OrderItem.java
@@ -8,6 +8,7 @@ public class OrderItem {
 
     public OrderItem(Menu menu, int quantity) {
         this.menu = menu;
+        validateQuantity(quantity);
         this.quantity = quantity;
     }
 
@@ -21,5 +22,22 @@ public class OrderItem {
 
     public int calculateAmount() {
         return menu.getPrice() * quantity;
+    }
+
+    private void validateQuantity(int quantity) {
+        isQuantityOverLimit(quantity);
+        isQuantityUnderLimit(quantity);
+    }
+
+    private void isQuantityOverLimit(int quantity) {
+        if (quantity > 20) {
+            throw new IllegalArgumentException("[ERROR] 메뉴는 한 번에 최대 20개까지만 주문할 수 있습니다. 다시 입력해 주세요.");
+        }
+    }
+
+    private void isQuantityUnderLimit(int quantity) {
+        if (quantity < 1) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
     }
 }
