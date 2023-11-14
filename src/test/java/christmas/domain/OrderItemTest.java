0b6f25ee2c5a774697a6b4c73dc5675535b74bb7
swhyeon98 <smreo75@gmail.com>
2023-11-14 19:46:23 +0900

    test: 주문 수량 초과 관련 테스트 추가

--- src/test/java/christmas/domain/OrderItemTest.java
+++ src/test/java/christmas/domain/OrderItemTest.java
@@ -2,14 +2,17 @@ package christmas.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class OrderItemTest {
 
     @Test
     @DisplayName("정상적인 OrderItem 생성")
-    public void createOrderItem(){
+    public void createOrderItem() {
         //given
         Menu menu = new Menu("양송이수프", 8000);
         int quantity = 1;
@@ -20,5 +23,18 @@ class OrderItemTest {
         //then
         assertThat(orderItem.getMenu()).isEqualTo(menu);
         assertThat(orderItem.getQuantity()).isEqualTo(quantity);
+    }
+
+    @DisplayName("주문 수량 초과 대한 예외 처리")
+    @ValueSource(strings = {"21", "200", "32"})
+    @ParameterizedTest
+    public void overQuantity(String input) {
+        //given
+        Menu menu = new Menu("양송이수프", 8000);
+        int quantity = Integer.parseInt(input);
+
+        //then
+        assertThatThrownBy(() -> new OrderItem(menu, quantity))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
