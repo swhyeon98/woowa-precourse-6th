31c88505cb37730efeda3f47c20d4827e124d8bf
swhyeon98 <smreo75@gmail.com>
2023-11-14 18:09:33 +0900

    test: 메뉴 이름 찾는 기능 테스트 추가

---
+++ src/test/java/christmas/repository/MenuRepositoryTest.java
@@ -0,0 +1,37 @@
+package christmas.repository;
+
+import christmas.domain.Menu;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class MenuRepositoryTest {
+
+    private MenuRepository menuRepository;
+
+    @BeforeEach
+    void setUp() {
+        menuRepository = new MenuRepository();
+    }
+
+    @ParameterizedTest
+    @DisplayName("메뉴 이름으로 메뉴 찾기")
+    @ValueSource(strings = {"양송이수프", "타파스", "시저샐러드", "티본스테이크", "바비큐립", "해산물파스타",
+            "크리스마스파스타", "초코케이크", "아이스크림", "제로콜라", "레드와인", "샴페인"})
+    public void findByAllMenu(String menuNames) {
+        //when
+        Optional<Menu> result = menuRepository.findByName(menuNames);
+
+        //then
+        assertThat(result.get().getName()).isEqualTo(menuNames);
+    }
+
+}
