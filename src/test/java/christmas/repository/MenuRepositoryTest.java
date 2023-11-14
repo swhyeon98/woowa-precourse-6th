580a46eb3a8186ca9cb773984271f6b9bffdc726
swhyeon98 <smreo75@gmail.com>
2023-11-14 18:12:48 +0900

    refactor: 사용하지 않는 import문 제거

--- src/test/java/christmas/repository/MenuRepositoryTest.java
+++ src/test/java/christmas/repository/MenuRepositoryTest.java
@@ -1,14 +1,12 @@
 package christmas.repository;
 
 import christmas.domain.Menu;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
