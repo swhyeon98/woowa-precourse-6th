fb944f9bd14fdf3765fd4f5b9ade43b9966aaeea
swhyeon98 <smreo75@gmail.com>
2023-11-16 00:01:51 +0900

    feat: 출력에 필요한 기능들 추가

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -37,4 +37,33 @@ public class OutputView {
     private String formatPrice(int price) {
         return String.format("%,d원", price);
     }
+
+    public void printOrderItems(List<String> orderedItems) {
+        System.out.println("<주문 메뉴>");
+        orderedItems.forEach(System.out::println);
+    }
+
+    public void printTotalBeforeDiscount(int totalBeforeDiscount) {
+        System.out.println("\n<할인 전 총주문 금액>\n" + formatPrice(totalBeforeDiscount));
+    }
+
+    public void printGiftItem(String giftItem) {
+        System.out.println("\n<증정 메뉴>\n" + giftItem);
+    }
+
+    public void printBenefits(String benefits) {
+        System.out.println("\n<혜택 내역>\n" + benefits);
+    }
+
+    public void printTotalDiscount(int totalDiscount) {
+        System.out.println("\n<총혜택 금액>\n" + formatPrice(-totalDiscount));
+    }
+
+    public void printTotalAfterDiscount(int totalAfterDiscount) {
+        System.out.println("\n<할인 후 예상 결제 금액>\n" + formatPrice(totalAfterDiscount));
+    }
+
+    public void printEventBadge(String eventBadge) {
+        System.out.println("\n<12월 이벤트 배지>\n" + eventBadge);
+    }
 }
