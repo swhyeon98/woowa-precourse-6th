5ecd63d55fb5fd5c0e7cd19b502c02310bd626ef
swhyeon98 <smreo75@gmail.com>
2023-11-15 19:01:48 +0900

    test: 12월 1일 할인 금액 테스트 추가

---
+++ src/test/java/christmas/domain/discount/XmasDdayDiscountPolicyTest.java
@@ -0,0 +1,37 @@
+package christmas.domain.discount;
+
+import christmas.domain.Order;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+class XmasDdayDiscountPolicyTest {
+
+    private DiscountPolicy discountPolicy;
+    private Order order;
+
+    //시작일인 12월 1일에 1,000원, 2일에 1,100원, ..., 25일엔 3,400원 할인)
+
+    @BeforeEach
+    void setUp() {
+        discountPolicy = new XmasDdayDiscountPolicy();
+        order = new Order();
+    }
+
+    @Test
+    @DisplayName("12월 1일 할인 금액 테스트")
+    void discountDecemberFirst(){
+        //given
+        LocalDate date = LocalDate.of(2023, Month.DECEMBER, 1);
+
+        //when
+        int discount = discountPolicy.discount(order, date);
+
+        //then
+        Assertions.assertThat(discount).isEqualTo(1000);
+    }
+}
