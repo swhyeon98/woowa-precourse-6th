afb8bfe5b4dc51fc6987ad0cce96aea994db86f5
swhyeon98 <smreo75@gmail.com>
2023-11-15 20:36:26 +0900

    test: 할인 미적용 테스트 추가 - 평일이지만 메인 메뉴일 때와 평일이 아닐 때 추가

--- src/test/java/christmas/domain/discount/WeekdayDiscountPolicyTest.java
+++ src/test/java/christmas/domain/discount/WeekdayDiscountPolicyTest.java
@@ -30,12 +30,42 @@ class WeekdayDiscountPolicyTest {
         //given
         Order order = new Order();
         order.addOrderItem(new OrderItem("초코케이크", 1));
-        LocalDate friday = LocalDate.of(2023, Month.DECEMBER, 4);
+        LocalDate monday = LocalDate.of(2023, Month.DECEMBER, 4);
+
+        //when
+        int discount = discountPolicy.discount(order, monday);
+
+        //then
+        assertThat(discount).isEqualTo(2023);
+    }
+
+    @Test
+    @DisplayName("평일이지만 메인 메뉴일 때, 할인 미적용")
+    public void weekdayMainNoDiscount(){
+        //given
+        Order order = new Order();
+        order.addOrderItem(new OrderItem("티본스테이크", 1));
+        LocalDate monday = LocalDate.of(2023, Month.DECEMBER, 4);
+
+        //when
+        int discount = discountPolicy.discount(order, monday);
+
+        //then
+        assertThat(discount).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("평일이 아닐 때, 할인 미적용")
+    public void notWeekDay(){
+        //given
+        Order order = new Order();
+        order.addOrderItem(new OrderItem("초코케이크", 1));
+        LocalDate friday = LocalDate.of(2023, Month.DECEMBER, 1);
 
         //when
         int discount = discountPolicy.discount(order, friday);
 
         //then
-        assertThat(discount).isEqualTo(2023);
+        assertThat(discount).isEqualTo(0);
     }
 }
