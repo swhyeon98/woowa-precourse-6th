ca605bfbfbfd7e972325035fc2743030ffe8ecc5
swhyeon98 <smreo75@gmail.com>
2023-11-15 18:35:14 +0900

    feat: 전략 패턴 적용한 할인 정책 인터페이스 추가

---
+++ src/main/java/christmas/domain/discount/DiscountPolicy.java
@@ -0,0 +1,10 @@
+package christmas.domain.discount;
+
+import christmas.domain.Order;
+
+import java.time.LocalDate;
+
+public interface DiscountPolicy {
+
+    int discount(Order order, LocalDate date);
+}
