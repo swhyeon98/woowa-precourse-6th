56fa5321871db5d0f8dd30ee87500efbc725814a
swhyeon98 <smreo75@gmail.com>
2023-11-15 17:07:08 +0900

    feat: 중복 주문 항목 검증 로직 추가

--- src/main/java/christmas/domain/Order.java
+++ src/main/java/christmas/domain/Order.java
@@ -1,7 +1,9 @@
 package christmas.domain;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Order {
 
@@ -9,5 +11,20 @@ public class Order {
 
     public Order() {
         this.orderItems = new ArrayList<>();
+    }
+
+    public void addOrderItems(List<OrderItem> newOrderItems) {
+        validateNoDuplicate(newOrderItems);
+        orderItems.addAll(newOrderItems);
+    }
+
+    private void validateNoDuplicate(List<OrderItem> newOrderItems) {
+        long uniqueCount = newOrderItems.stream()
+                .map(orderItem -> orderItem.getMenu().getName())
+                .distinct()
+                .count();
+        if (uniqueCount != newOrderItems.size()) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
     }
 }
