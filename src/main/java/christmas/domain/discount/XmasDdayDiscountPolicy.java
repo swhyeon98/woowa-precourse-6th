b8da2f871938ea54f6b590ebc02a980a6e866e2e
swhyeon98 <smreo75@gmail.com>
2023-11-15 19:00:09 +0900

    feat: 26일 이상은 할인 정책이 적용되지 않도록 조건 추가

--- src/main/java/christmas/domain/discount/XmasDdayDiscountPolicy.java
+++ src/main/java/christmas/domain/discount/XmasDdayDiscountPolicy.java
@@ -9,7 +9,7 @@ public class XmasDdayDiscountPolicy implements DiscountPolicy {
 
     @Override
     public int discount(Order order, LocalDate date) {
-        if (date.getMonth() == Month.DECEMBER) {
+        if (date.getMonth() == Month.DECEMBER && date.getDayOfMonth() <= 25) {
             return 1000 + (date.getDayOfMonth() - 1) * 100;
         }
         return 0;
