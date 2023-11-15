7bf28daddcbac68c71c8a52e1eee008f70f29f66
swhyeon98 <smreo75@gmail.com>
2023-11-15 21:51:43 +0900

    feat: 카테고리 enum에 한글 표시 이름 추가

--- src/main/java/christmas/domain/Category.java
+++ src/main/java/christmas/domain/Category.java
@@ -1,5 +1,19 @@
 package christmas.domain;
 
 public enum Category {
-    APPETIZER, MAIN, DESSERT, DRINK
+    APPETIZER("애피타이저"),
+    MAIN("메인"),
+    DESSERT("디저트"),
+    DRINK("음료");
+
+    private final String displayName;
+
+    Category(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
 }
+
