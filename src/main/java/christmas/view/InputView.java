ed083e52e9ea5a5c5b44ca0a6e4a3337ddec38cc
swhyeon98 <smreo75@gmail.com>
2023-11-15 21:40:39 +0900

    feat: 메뉴 입력 기능 추가

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -6,7 +6,15 @@ public class InputView {
 
     public int readDate() {
         System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
-        String input = Console.readLine();
-        return Integer.parseInt(input);
+        return Integer.parseInt(readInput());
+    }
+
+    public String readMenu() {
+        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+        return readInput();
+    }
+
+    private String readInput() {
+        return Console.readLine();
     }
 }
